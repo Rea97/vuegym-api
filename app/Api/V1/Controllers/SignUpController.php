46e3c2cf1dec522b2c040f078de32d9568baf47e
Oziel Martinez <ozielrea97@hotmail.com>
2018-07-14 01:03:34 -0500

    WIP

--- app/Api/V1/Controllers/SignUpController.php
+++ app/Api/V1/Controllers/SignUpController.php
@@ -4,6 +4,8 @@ namespace App\Api\V1\Controllers;
 
 use Config;
 use App\User;
+use Illuminate\Log\Logger;
+use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\JWTAuth;
 use App\Http\Controllers\Controller;
 use App\Api\V1\Requests\SignUpRequest;
@@ -11,12 +13,21 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SignUpController extends Controller
 {
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function signUp(SignUpRequest $request, JWTAuth $JWTAuth)
     {
         $user = new User($request->all());
         if(!$user->save()) {
             throw new HttpException(500);
         }
+
+        $this->logger->debug("New user '{$user->name} was created.'");
 
         if(!Config::get('boilerplate.sign_up.release_token')) {
             return response()->json([
@@ -27,6 +38,7 @@ class SignUpController extends Controller
         $token = $JWTAuth->fromUser($user);
         return response()->json([
             'status' => 'ok',
+            'user' => $user,
             'token' => $token
         ], 201);
     }
